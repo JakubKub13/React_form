010149b16cc30feea6ddfa6b1b9e87d06ab7290a
JakubKub13 <jakubkubala3@gmail.com>
2023-07-05 09:24:33 +0200

    Creating of form

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [users, setUsers] = useState([])
+
+  const formSubmit = (e) => {
+    e.preventDefault()
+    
+  }
+
+  return <article>
+    <form onSubmit={formSubmit}>
+        <input className="userInfo" type="text" placeholder="Name" />
+        <input className="userInfo" type="email" placeholder='Email' />
+        <input type="submit"/>
+    </form>
+  </article>
+}
+
+export default App
